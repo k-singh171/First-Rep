bebec561b241bdc7e0b0d8f0b8f8191c4de41f61
Ashu <singhat99055@gmail.com>
2025-12-01 19:42:34 +0530

    gitLab commit

---
+++ gitLab.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class gitLab {
+    public static void main(String[] args) {
+        int ageing = 20;
+        System.out.println("Age: " + ageing);
+        ArrayList<Integer> ageList = new ArrayList<>();
+        ageList.add(50);
+        ageList.add(24);
+        ageList.add(45);
+        ageList.add(34);
+        System.out.println();
+        NamingDesk obj = new NamingDesk();
+        obj.name();
+        System.out.println();
+        School obj1 = new School();
+        obj1.sclName();
+    }
+}
+
+class NamingDesk{
+    public String name(){
+        return "Naming help desk available";
+    }
+}
+class School{
+    public void sclName(){
+        System.out.println("Narayana Vidyalayam");
+    }
+}
