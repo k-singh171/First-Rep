eb4cb71f317cecc9517c59786c0817f6750f49dc
Ashu <singhat99055@gmail.com>
2025-12-01 18:01:20 +0530

    Second Commit

--- gitPrac.java
+++ gitPrac.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class gitPrac {
     public static void main(String[] args) {
         int value = 10;
+        String name = "Ashutosh";
+        System.out.println("Name: " + name);
+        ArrayList<String> staffName = new ArrayList<>();
+        staffName.add("Ashu");
+        staffName.add("Pratham");
+        staffName.add("Abhinash");
+        System.out.println();
+        Phone obj = new Phone();
+        obj.ring();
+    }
+}
+class Phone{
+    public void ring(){
+        System.out.println("Ringing...");
     }
 }
